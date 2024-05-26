0754b93607f21c31ee1ac97f038f0db5b0a5ce39
Marcin Bieda <1736293+mbieda@users.noreply.github.com>
2024-05-26 11:23:18 +0200

    Added public init for PlanetConfig

--- Sources/PlanetOnboarding/PlanetConfig.swift
+++ Sources/PlanetOnboarding/PlanetConfig.swift
@@ -4,4 +4,10 @@ public struct PlanetConfig: Hashable {
     let backgroundColor: Color
     let title: String
     let satelliteColor: Color
+    
+    public init(backgroundColor: Color, title: String, satelliteColor: Color) {
+        self.backgroundColor = backgroundColor
+        self.title = title
+        self.satelliteColor = satelliteColor
+    }
 }
